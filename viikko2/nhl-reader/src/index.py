65784580742f1f041acbd7e2b828e2ba42fcca07
Kauri-Viljar <macabre.cs@gmail.com>
2025-11-05 10:28:14 +0200

    Tehtävä 4. Pelaajalistan refaktorointi

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,24 +1,16 @@
-import requests
-from player import Player
+from player_reader import PlayerReader
+from statistic_service import PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
     nationality = input("Minkä maan pelaajat tulostetaan?")
-
-    players.sort(key=lambda p: p.points(), reverse=True)
+    players = stats.top_scorers_by_nationality(nationality)
 
     print(f"\nPlayers from {nationality}:\n")
     for player in players:
-        if player.nationality == nationality:
-            print(player)
+        print(player)
 
 if __name__ == "__main__":
     main()
