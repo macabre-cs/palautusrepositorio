bad77368964284e484ce1a129061c41640c3a1dd
Kauri-Viljar <macabre.cs@gmail.com>
2025-11-18 09:42:29 +0200

    Metodi tyhjenna tyhjentää korin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -115,3 +115,15 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(len(self.kori.ostokset()), 0)
+    
+    def test_ostoskori_tyhjenee(self):
+        maito = Tuote("Maito", 3)
+        pullapitko = Tuote("Pullapitko", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(pullapitko)
+
+        self.kori.tyhjenna()
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(len(self.kori.ostokset()), 0)
