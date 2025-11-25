cfff5f9e90425918b7c0a62ce23d59bfa8db0275
Kauri-Viljar <macabre.cs@gmail.com>
2025-11-25 09:19:17 +0200

    refaktoroi get_score-metodi jakamalla se pienempiin metodeihin

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -16,20 +16,24 @@ class TennisGame:
         elif player_name == self.player2_name:
             self.score2 += 1
 
+    def _equal_score(self):
+        if self.score1 < 3:
+            return f"{self.score_name(self.score1)}-All"
+        return "Deuce"
+
+    def _advantage_or_win(self):
+        diff = self.score1 - self.score2
+        if diff == 1:
+            return "Advantage player1"
+        if diff == -1:
+            return "Advantage player2"
+        if diff >= 2:
+            return "Win for player1"
+        return "Win for player2"
+
     def get_score(self):
         if self.score1 == self.score2:
-            if self.score1 < 3:
-                return f"{self.score_name(self.score1)}-All"
-            return "Deuce"
-
+            return self._equal_score()
         if self.score1 >= 4 or self.score2 >= 4:
-            diff = self.score1 - self.score2
-            if diff == 1:
-                return "Advantage player1"
-            if diff == -1:
-                return "Advantage player2"
-            if diff >= 2:
-                return "Win for player1"
-            return "Win for player2"
-
+            return self._advantage_or_win()
         return f"{self.score_name(self.score1)}-{self.score_name(self.score2)}"
